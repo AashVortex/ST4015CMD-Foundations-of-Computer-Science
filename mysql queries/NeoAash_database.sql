e9b4107616718772b61953f9fab0086f462cebfb
Aashish singh <123aashishdona@gmail.com>
2026-02-21 00:02:19 +0545

    Create NeoAash database and initial DATA table

--- mysql queries/NeoAash_database.sql
+++ mysql queries/NeoAash_database.sql
@@ -3,3 +3,15 @@ CREATED DATABASE NeoAash;
 
 USE NeoAash;
 
+
+-- Part A: Unnormalized Table
+CREATE TABLE DATA (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    StudentID INT,
+    StudentName VARCHAR(50),
+    Email VARCHAR(50),
+    ClubName VARCHAR(50),
+    ClubRoom VARCHAR(50),
+    ClubMentor VARCHAR(50),
+    JoinDate DATE
+);
