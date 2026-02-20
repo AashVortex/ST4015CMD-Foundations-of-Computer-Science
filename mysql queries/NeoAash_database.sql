5496c3f5fa241ae541b7c49e06d51d893f6c449d
Aashish singh <123aashishdona@gmail.com>
2026-02-21 01:09:03 +0545

    Add comment on 1NF normalization in SQL file
    Added comments regarding normalization status.

--- mysql queries/NeoAash_database.sql
+++ mysql queries/NeoAash_database.sql
@@ -27,3 +27,5 @@ INSERT INTO DATA (StudentID, StudentName, Email, ClubName, ClubRoom, ClubMentor,
 (6, 'Pooja', 'pooja@email.com', 'Sports Club', 'R202', 'Ms. Sita', '2024-01-27'),
 (3, 'Nisha', 'nisha@email.com', 'Coding Club', 'Lab1', 'Mr. Anil', '2024-01-28'),
 (7, 'Aman', 'aman@email.com', 'Coding Club', 'Lab1', 'Mr. Anil', '2024-01-30');
+
+--it's already in 1NF so don't have to do normalization of 1NF 
