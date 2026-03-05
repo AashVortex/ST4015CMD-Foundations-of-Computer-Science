e6538eb8f324560bf52d8b1793495f502752bbc1
Aashish singh <123aashishdona@gmail.com>
2026-03-05 18:57:39 +0545

    Delete TASK 1/script/URL encoding.cmd

--- TASK 1/script/URL encoding.cmd
+++
@@ -1,3 +0,0 @@
-PS C:\Users\LENOVO> docker run --rm python:3.12 python -c "import urllib.parse; print(urllib.parse.quote('hello world to everyone'))"
-hello%20world%20to%20everyone
-PS C:\Users\LENOVO>
